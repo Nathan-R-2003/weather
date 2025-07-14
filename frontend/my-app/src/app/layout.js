6ba2076654c018633f2d5966d248d575a725454d
Nathan <nathanrodriguez240@gmail.com>
2025-07-14 16:00:39 -0400

    dumb problem fix

---
+++ frontend/my-app/src/app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "ClearCast",
+  description: "Stay one step ahead of the weather with ClearCast — your sleek, reliable, and easy-to-use weather companion. Whether you're planning your day, your week, or your next adventure, ClearCast gives you accurate, real-time forecasts tailored to your location.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
