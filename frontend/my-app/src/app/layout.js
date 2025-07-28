e565e873ac6bdfa04f4e254a2061ff2023e9e73c
Nathan <nathanrodriguez240@gmail.com>
2025-07-27 23:35:56 -0400

    managed to obtain user's city for default forecast

--- frontend/my-app/src/app/layout.js
+++ frontend/my-app/src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "ClearCast",
+  title: "Clear Cast",
   description: "Stay one step ahead of the weather with ClearCast — your sleek, reliable, and easy-to-use weather companion. Whether you're planning your day, your week, or your next adventure, ClearCast gives you accurate, real-time forecasts tailored to your location.",
 };
 
--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,6 +1,37 @@
 import Image from "next/image";
+import navigator from "react";
+import axios from 'axios';
 
 export default function Home() {
   
-    
+	//Defaults coords to Orlando 
+	const city = "Orlando";
+	const location = {lat: 28.538336, 
+					  lon: -81.379234}
+
+	if('geolocation' in navigator){
+		
+		navigator.geolocation.getCurrentPosition(position => {
+			location.lat = position.coords.lat;
+			location.lon = position.coords.lon
+		});
+
+		getUserCity();
+	}
+	
+	const getUserCity = async () => {
+		const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${location.lat}&lon=${location.lon}&format=json`, {
+      		headers: {
+        			'User-Agent': 'Clear Cast', // Nominatim requires this!
+       				'Accept-Language': 'en',
+     				},
+		}).catch(e => {
+			console.log(e);
+		});
+
+		const data = await response.json();
+		console.log(data);
+		city = data.address.city;
+	};
+	
 }
