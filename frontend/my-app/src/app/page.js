a15c314471aa2087e20ffa2cad5c58ce326a220a
Nathan <nathanrodriguez240@gmail.com>
2025-08-30 21:30:30 -0400

    figured out weather api, now I just need to filter through the data

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,14 +1,15 @@
 'use client'
 
 import Image from "next/image";
+import {useState, useEffect} from "react";
 
 
 export default function Home() {
   
 	//Defaults coords to Orlando 
-	var city = "Orlando";
-	var location = {lat: 28.538336, 
-					lon: -81.379234}
+	const [city, setCity] = useState("Orlando");
+	const [lat, setLat] = useState(28.538336);
+	const [lon, setLon] = useState(-81.379234);
 
 	//function to get user's city based on coordinates
 	const getUserCity = async (lat, lon) => {
@@ -21,20 +22,45 @@ export default function Home() {
   		.catch(error => console.log('error', error));
 
 		const data = await response.json();
-		city = data.features[0].properties.city;
+		setCity(data.features[0].properties.city);
 	};
 
-	
-	if('geolocation' in navigator){
+	useEffect(() => {
+		if('geolocation' in navigator){
 		
-		navigator.geolocation.getCurrentPosition(position => {
-			location.lat = position.coords.latitude;
-			location.lon = position.coords.longitude;
-		});
+			navigator.geolocation.getCurrentPosition(position => {
+				setLat(position.coords.latitude)
+				setLon(position.coords.longitude);
+			});
 
-		getUserCity(location.lat, location.lon);
+			getUserCity(lat, lon);
+		}
+
+		getForeCast(lat, lon)
+	})
+
+	const getForeCast = async (lat, lon) => {
+
+		var requestOptions = {
+  			method: 'GET',
+		};
+
+		const response = await fetch(`https://api.weather.gov/points/${lat},${lon}`, requestOptions)
+		.catch(error => console.log('error', error));
+
+		const data = await response.json();
+
+		const hourlyForecast = await fetch(data.properties.forecastHourly, requestOptions)
+		.catch(error => console.log('error', error));
+
+		const hourlyForecastData = await hourlyForecast.json();
+
+		console.log(hourlyForecastData.properties.periods);
+
 	}
-
 	
+	return(
+		<p>{city}</p>
+	)
 	
 }
