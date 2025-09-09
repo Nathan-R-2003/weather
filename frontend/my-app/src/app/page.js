54ccfb621e63ade382fba72b6ffbd18601cec98b
Nathan <nathanrodriguez240@gmail.com>
2025-09-08 21:20:37 -0400

    implemented a search bar for cities using geoapify address autocomplete, just need to enable clicking on the address to get the forecast

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -2,14 +2,21 @@
 
 import Image from "next/image";
 import {useState, useEffect} from "react";
+import '@geoapify/geocoder-autocomplete/styles/round-borders.css';
+import {
+  GeoapifyGeocoderAutocomplete,
+  GeoapifyContext
+} from '@geoapify/react-geocoder-autocomplete';
+
 
 
 export default function Home() {
   
 	//Defaults coords to Orlando 
-	const [city, setCity] = useState("Orlando");
+	const [city, setCity] = useState("Orlando, FL");
 	const [lat, setLat] = useState(28.538336);
 	const [lon, setLon] = useState(-81.379234);
+	const [searchValue, setSearchValue] = useState("");
 
 	//function to get user's city based on coordinates
 	const getUserCity = async (lat, lon) => {
@@ -22,7 +29,7 @@ export default function Home() {
   		.catch(error => console.log('error', error));
 
 		const data = await response.json();
-		setCity(data.features[0].properties.city);
+		setCity(data.features[0].properties.city + ", " + data.features[0].properties.state_code);
 	};
 
 	useEffect(() => {
@@ -58,9 +65,32 @@ export default function Home() {
 		console.log(hourlyForecastData.properties.periods);
 
 	}
+
+	const onPlaceSelect = async () => {
+		
+	}
 	
 	return(
-		<p>{city}</p>
+		<div className="main-container">
+			<div className="small-display">
+				<div className="search-container">
+					<GeoapifyContext apiKey={process.env.NEXT_PUBLIC_REVERSE_GEOCODING_API_KEY}>
+						<GeoapifyGeocoderAutocomplete
+							placeholder="Enter city here"
+							skipIcons={true}
+							debounceDelay={600}
+							skipSelectionOnArrowKey={true}
+							value={searchValue}
+							filterByCountryCode={['us']}
+							placeSelect={onPlaceSelect}
+						/>
+					</GeoapifyContext>
+				</div>
+			</div>
+			<div className="big-display">
+
+			</div>
+		</div>
 	)
 	
 }
